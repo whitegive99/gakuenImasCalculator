c000ab1c3779144b9620832f9ef42bfb920026aa
whitegive99 <1341338291@qq.com>
2024-06-16 01:35:35 +0900

    更新

--- __init__.py
+++ __init__.py
@@ -2,7 +2,10 @@ from hoshino import Service
 from hoshino.typing import CQEvent
 from .calc import calc_rank
 
-sv = Service('学园偶像大师算分插件', enable_on_default=True, bundle='娱乐')
+sv = Service('学园偶像大师算分插件', help_='''
+[算分] + 三维属性
+例如：算分 1200 1300 1400
+'''.strip())
 
 @sv.on_prefix('算分')
 async def handle_calc_rank(bot, ev: CQEvent):
